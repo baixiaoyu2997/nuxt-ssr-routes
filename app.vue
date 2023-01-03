7ec47ae88751859c180c7afd73fc9d7a2afd7616
白晓宇 <baixiaoyu@yeeyuntech.com>
2023-01-03 11:10:00 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div>123</div>
+    <p>Fetched from <pre>/api/mountain</pre>: {{ mountain }}</p>
+  </div>
+    
+</template>
+
+<script setup>
+const { data: mountain } = await useFetch('/api/mountain')
+</script>
