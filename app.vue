ace4a81d95943c470c4985f782d6a7c8d64dfa76
白晓宇 <baixiaoyu@yeeyuntech.com>
2023-01-03 11:14:37 +0800

    test

--- app.vue
+++ app.vue
@@ -2,10 +2,14 @@
   <div>
     <div>123</div>
     <p>Fetched from <pre>/api/mountain</pre>: {{ mountain }}</p>
+    <button @click="onClick">Fetch</button>
   </div>
     
 </template>
 
 <script setup>
 const { data: mountain } = await useFetch('/api/mountain')
+const onClick=async ()=>{
+  const { data: test } = await useFetch('/api/test')
+}
 </script>
---
+++ server/api/test.js
@@ -0,0 +1,5 @@
+export default defineEventHandler(() => {
+  return {
+    text: "你好",
+  };
+});
